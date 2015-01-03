9c551bd92fd3c1039cd1585edf923d77be5d4bc8
maxisoft <maxisoft.eu@gmail.com>
2015-01-03 15:34:15 +0100

    init

---
+++ src/main/java/ga/maxisoft/steam/wishlist/SteamGame.java
@@ -0,0 +1,14 @@
+package ga.maxisoft.steam.wishlist;
+
+public class SteamGame {
+    private final long id;
+    private final String name;
+
+
+    public SteamGame(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+}
---
+++ src/main/java/ga/maxisoft/steam/wishlist/WhishList.java
@@ -0,0 +1,4 @@
+package ga.maxisoft.steam.wishlist;
+
+public class WhishList {
+}
